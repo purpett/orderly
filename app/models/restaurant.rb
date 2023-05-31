582816783704dfcaeb91225b0f7b853b32f97368
Michela Bicocchi <mbicocchi1@gmail.com>
2023-05-31 14:38:22 +0100

    Install devise

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :menu_sections
   has_many :orders
 end
--- db/migrate/20230531111027_create_restaurants.rb
+++ db/migrate/20230531111027_create_restaurants.rb
@@ -2,8 +2,6 @@ class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :email
-      t.string :password
       t.string :location
 
       t.timestamps

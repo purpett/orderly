72ef6aa4aa9786bd9310f14a2c799021f51a377e
Michela Bicocchi <mbicocchi1@gmail.com>
2023-05-31 13:02:07 +0100

    Create Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_115939) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_120115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "price"
+    t.bigint "menu_section_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["menu_section_id"], name: "index_items_on_menu_section_id"
+  end
 
   create_table "menu_sections", force: :cascade do |t|
     t.string "name"
@@ -31,5 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_115939) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "menu_sections"
   add_foreign_key "menu_sections", "restaurants"
 end

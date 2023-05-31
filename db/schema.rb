eaade9b4675bcbc5f241e45afbe208ad3310c9f2
Michela Bicocchi <mbicocchi1@gmail.com>
2023-05-31 15:21:20 +0100

    Install FriendlyId

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_133633) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_142106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -59,8 +70,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_133633) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "slug"
     t.index ["email"], name: "index_restaurants_on_email", unique: true
     t.index ["reset_password_token"], name: "index_restaurants_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_restaurants_on_slug", unique: true
   end
 
   add_foreign_key "items", "menu_sections"

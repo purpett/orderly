e2c79f0285cf0b510801cee39fd4a24ef38ace85
Michela Bicocchi <mbicocchi1@gmail.com>
2023-06-02 16:06:57 +0100

    Add js for navbar mobile toggles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,5 +5,21 @@ import "./controllers"
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root"))
-root.render(<div>Hello</div>)
+// const root = createRoot(document.getElementById("root"))
+// root.render(<div>Hello</div>)
+
+document.addEventListener("turbo:load", () => {
+  const hamburger = document.querySelector("#mobile-nav-hamburger");
+  const mobileNavClose = document.querySelector("#mobile-nav-close");
+  const mobileNav = document.querySelector("#mobile-nav");
+
+  if (hamburger) {
+    hamburger.addEventListener("click", () => {
+      mobileNav.classList.remove("hidden");
+    });
+
+    mobileNavClose.addEventListener("click", () => {
+      mobileNav.classList.add("hidden");
+    })
+  }
+})

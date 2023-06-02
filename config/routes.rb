b87552ad05dc88a4685439cedf3f4c56bdcffc71
Michela Bicocchi <mbicocchi1@gmail.com>
2023-06-02 15:50:55 +0100

    Add name field to devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  devise_scope :restaurant do
-    root to: "devise/sessions#new"
-  end
+  root to: "restaurants#show"
 
   resources :menu_sections do
     resources :items

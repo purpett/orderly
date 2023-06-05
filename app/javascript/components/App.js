c6c6cc1b3f7663084d43103cd27b2cd71847fd31
Michela Bicocchi <mbicocchi1@gmail.com>
2023-06-05 16:37:10 +0100

    Implement local storage to save customer and order details

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,44 @@
 import React, { useState, useEffect } from "react"
-import { Routes, Route, useParams } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import CustomerDetails from "./CustomerDetails"
 import Menu from "./Menu"
 import Order from "./Order"
 
+function loadCustomerInfo() {
+  const customerInfo = JSON.parse(localStorage.getItem("customerInfo"))
+
+  if (customerInfo) {
+    return customerInfo
+  }
+
+  return {
+    name: "",
+    email: "",
+    phone: ""
+  }
+}
+
+function loadOrder() {
+  const order = JSON.parse(localStorage.getItem("order"))
+
+  if (order) {
+    return order
+  }
+
+  return []
+}
+
 export default function App() {
-  const [customerInfo, setCustomerInfo] = useState({})
-  const [order, setOrder] = useState([])
+  const [customerInfo, setCustomerInfo] = useState(loadCustomerInfo())
+  const [order, setOrder] = useState(loadOrder())
+
+  useEffect(() => {
+    localStorage.setItem("order", JSON.stringify(order))
+  }, [order])
+
+  useEffect(() => {
+    localStorage.setItem("customerInfo", JSON.stringify(customerInfo))
+  }, [customerInfo])
 
   function addItemToOrder(item) {
     setOrder([...order, item])
